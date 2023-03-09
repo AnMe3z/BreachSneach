0cd0372ce2483bf27ab06ab9986455e60898c6ff
milchevdimitar <85071480+milchevdimitar@users.noreply.github.com>
2023-03-09 16:28:59 +0200

    chakra

--- FrontEnd/pages/index.js
+++ FrontEnd/pages/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const regex = new RegExp(/\.\/images\//);
 
@@ -9,11 +10,12 @@ export default class App extends Component {
   render() {
     return (
       <ChakraProvider>
-        <div>Logo</div>
-        <Image 
-          src="https://raw.githubusercontent.com/AnMe3z/BreachSneach/main/FrontEnd/pages/images/Logo.jpg"
-          boxSize="300px"
-        />
+        <Flex h="100vh" alignItems="center" justifyContent="center">
+          <Image 
+            src="https://raw.githubusercontent.com/AnMe3z/BreachSneach/main/FrontEnd/pages/images/Logo.jpg"
+            boxSize="332px x 217px"
+          />
+          <Button colorScheme="blackAlpha" variant="outline" color="#FFFFFF" borderColor="#32127A" bg="#000000">Get started</Button>        </Flex>
       </ChakraProvider>
     );
   }
