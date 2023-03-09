ba3bd8b79ddcc10432f774f31248ca883a7d31a9
milchevdimitar <85071480+milchevdimitar@users.noreply.github.com>
2023-03-09 18:20:30 +0200

    login

--- FrontEnd/pages/index.js
+++ FrontEnd/pages/index.js
@@ -7,7 +7,7 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
 const regex = new RegExp(/\.\/images\//);
 
 const handleButtonClick = () => {
-  window.location.href = "";
+  window.location.href = "FrontEnd/pages/login.html";
 };
 
 export default class App extends Component {
